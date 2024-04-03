9e64e92a20f8f837b52e2ace25ba1da47dbed5fa
Christian Cook <3473396+CookieCookson@users.noreply.github.com>
2024-04-03 21:25:05 +0100

    fix: Handle fetch requests and responses for different payload types

--- src/adapters/fetchAdapter.ts
+++ src/adapters/fetchAdapter.ts
@@ -1,17 +1,36 @@
 import { AdapterFunction } from ".";
 
-const fetchAdapter: AdapterFunction = ({ url, method, data, headers }) => {
-  return fetch(url, {
+const fetchAdapter: AdapterFunction = async ({
+  url,
+  method,
+  data,
+  headers,
+}) => {
+  let body: any = data;
+  const contentType: string | undefined = headers["Content-Type"];
+  if (contentType === "application/json") {
+    body = JSON.stringify(body);
+  }
+  const response = await fetch(url, {
     method,
-    body: data,
+    body,
     headers,
-  }).then(({ headers, status, json }) => {
-    return json().then((data) => ({
-      data,
-      headers,
-      status,
-    }));
   });
+  if (!response.ok) {
+    const err = await response.text();
+    throw new Error(err);
+  }
+  let text: any = await response.text();
+  if (typeof text === "string") {
+    try {
+      text = JSON.parse(text);
+    } catch {}
+  }
+  return {
+    data: text,
+    headers: Object.fromEntries(response.headers.entries()),
+    status: response.status,
+  };
 };
 
 export default fetchAdapter;
