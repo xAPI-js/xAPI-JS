6c22845e218077196924bc5aac399edab132f71e
Christian Cook <3473396+CookieCookson@users.noreply.github.com>
2024-04-08 17:59:46 +0100

    test: Fetch stringifies data if JSON content type

--- src/adapters/fetchAdapter.unit.test.ts
+++ src/adapters/fetchAdapter.unit.test.ts
@@ -22,4 +22,26 @@ describe("fetchAdapter", () => {
       })
     );
   });
+
+  it("stringifies JSON data", async () => {
+    fetchAdapter({
+      url: "https://www.example.com",
+      method: "POST",
+      data: { foo: true },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    expect(fetch).toHaveBeenCalledWith(
+      "https://www.example.com",
+      expect.objectContaining({
+        method: "POST",
+        body: '{"foo":true}',
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+    );
+  });
 });
